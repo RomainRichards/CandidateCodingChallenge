6652598795f90567179d74560fa014ad22f037ea
Romain Richards <96961208+RomainRichards@users.noreply.github.com>
2023-08-27 22:08:10 -0400

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './header/header.component'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule // import httpclientmodule to do http calls.
   ],
   providers: [],
   bootstrap: [AppComponent]
