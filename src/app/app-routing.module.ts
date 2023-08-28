d52bba912b83aff22b7675c547f314fb11ccfbb7
Romain Richards <96961208+RomainRichards@users.noreply.github.com>
2023-08-28 12:03:08 -0400

    add user-edit routing , updated navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './Pages/home-page/home-page.component';
 import { UserCreateComponent } from './Pages/user-create/user-create.component';
 import { UserAccountComponent } from './Pages/user-account/user-account.component';
+import { UserEditComponent } from './Pages/user-edit/user-edit.component';
 
 // Add routing for page navigation.
 const routes: Routes = [
   {path:'', component: HomePageComponent, title: 'Home Page'},
   {path:'user/create', component: UserCreateComponent, title: 'Create User'},
   {path:'users', component: UserAccountComponent, title: 'User Accounts'},
-  {path:'users/{{item.id}}/edit', component: UserAccountComponent, title: 'Edit User Account'}
+  {path:'users/:id/edit', component: UserEditComponent, title: 'Edit User'}
   
 ];
 
