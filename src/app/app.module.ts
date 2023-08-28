51c592e16768d0164434a0967045bd3496d0331e
Romain Richards <96961208+RomainRichards@users.noreply.github.com>
2023-08-27 22:31:42 -0400

    Add home and about pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HomePageComponent } from './Pages/home-page/home-page.component';
+import { AboutPageComponent } from './Pages/about-page/about-page.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomePageComponent,
+    AboutPageComponent,
 
   ],
   imports: [
